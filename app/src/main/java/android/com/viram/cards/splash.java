db355730971ad5247fe2bbaa5a3b943f6f474261
Viram308 <shahviram308@gmail.com>
2019-04-01 22:14:09 +0530

    Splash Activity Added

---
+++ app/src/main/java/android/com/viram/cards/splash.java
@@ -0,0 +1,28 @@
+package android.com.viram.cards;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Viram on 4/1/2019.
+ */
+
+public class splash extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+        final Runnable r = new Runnable() {
+            public void run() {
+                Intent i = new Intent(getApplicationContext(), MainActivity.class);
+                startActivity(i);
+                finish();
+            }
+        };
+        Handler h=new Handler();
+        h.postDelayed(r,2000);
+    }
+}
