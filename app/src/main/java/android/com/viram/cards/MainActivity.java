fc3855b5ba89e7074de3d240ab1df108e5cf73e4
Viram308 <shahviram308@gmail.com>
2019-04-01 14:51:52 +0530

    Initial commit

---
+++ app/src/main/java/android/com/viram/cards/MainActivity.java
@@ -0,0 +1,34 @@
+package android.com.viram.cards;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+public class MainActivity extends AppCompatActivity {
+
+    // Used to load the 'native-lib' library on application startup.
+    static {
+        System.loadLibrary("native-lib");
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+final FirebaseDatabase database = FirebaseDatabase.getInstance();
+        DatabaseReference ref= database.getReference();
+        ref.child("viram").setValue("VS");
+        // Example of a call to a native method
+
+
+    }
+
+    /**
+     * A native method that is implemented by the 'native-lib' native library,
+     * which is packaged with this application.
+     */
+    public native String stringFromJNI();
+}
