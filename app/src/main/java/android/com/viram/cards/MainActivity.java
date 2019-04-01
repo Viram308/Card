7f8df989f2f9939d880118890af57b7183d68620
Viram308 <shahviram308@gmail.com>
2019-04-01 15:03:51 +0530

    hg

--- app/src/main/java/android/com/viram/cards/MainActivity.java
+++ app/src/main/java/android/com/viram/cards/MainActivity.java
@@ -8,7 +8,7 @@ import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
 public class MainActivity extends AppCompatActivity {
-int f=1;
+int f=2;
     // Used to load the 'native-lib' library on application startup.
     static {
         System.loadLibrary("native-lib");
